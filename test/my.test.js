03f3e58129be6d0b6baec9cee5b38020a9ec0d64
Eliot <saltycrane@gmail.com>
2016-12-08 07:10:14 -0800

    moxios 0.2.0 doesn't work with axios 0.12.0

--- test/my.test.js
+++ test/my.test.js
@@ -1,44 +1,10 @@
 
-console.log("asdf");
-
 import axios from 'axios'
 import moxios from 'moxios'
 import sinon from 'sinon'
 import { equal } from 'assert'
 
-describe.only('mocking axios requests', function () {
-
-  describe('across entire suite', function () {
-
-    beforeEach(function () {
-      moxios.install()
-    })
-
-    afterEach(function () {
-      moxios.uninstall()
-    })
-
-    it('stub response for any matching request URL', function (done) {
-      // Match against an exact URL value
-      moxios.stubRequest('/say/hello', {
-        status: 200,
-        responseText: 'hello'
-      })
-
-      // Alternatively URL can be a RegExp
-      moxios.stubRequest(/say.*/, {/* ... */})
-
-      let onFulfilled = sinon.spy()
-      axios.get('/say/hello').then(onFulfilled)
-
-      moxios.wait(function () {
-        equal(onFulfilled.getCall(0).args[0].data, 'hello')
-        done()
-      })
-    })
-
-  })
-
+describe('mocking axios requests', function () {
   it('just for a single spec', function (done) {
     moxios.withMock(function () {
       let onFulfilled = sinon.spy()
@@ -58,5 +24,4 @@ describe.only('mocking axios requests', function () {
       })
     })
   })
-
 })
